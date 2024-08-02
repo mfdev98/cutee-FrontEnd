593bf963adf5582c27c2da078f5f7e1f0c93dc5b
MUKHAMMADYUSUF <mfdev98@gmail.com>
2024-08-02 20:30:32 +0900

    fix: retreive top users and modify ActiveUsers component

--- src/app/screens/homePage/ActiveUsers.tsx
+++ src/app/screens/homePage/ActiveUsers.tsx
@@ -4,45 +4,55 @@ import { CssVarsProvider, Typography } from "@mui/joy";
 import CardOverflow from "@mui/joy/CardOverflow";
 import AspectRatio from "@mui/joy/AspectRatio";
 
-const activeUsers = [
-   { memberNick: "Martin", memberImage: "/img/martin.webp" },
-   { memberNick: "Justin", memberImage: "/img/justin.webp" },
-   { memberNick: "Rose", memberImage: "/img/rose.webp" },
-   { memberNick: "Nusret", memberImage: "/img/nusret.webp" },
-];
+import { useSelector } from "react-redux";
+import { createSelector } from "reselect";
+import { retrieveTopUsers } from "./selector";
+import { serverApi } from "../../../lib/config";
+import { Member } from "../../../lib/types/member";
+
+/** REDUX SLICE & SELECTOR */
+const topUsersRetriever = createSelector(retrieveTopUsers, (topUsers) => ({
+  topUsers,
+}));
 
 export default function ActiveUsers() {
-   return (
-      <div className={"active-users-frame"}>
-         <Container>
-            <Stack className={"main"}>
-               <Box className={"category-title"}>Active Users</Box>
-               <Stack className={"cards-frame"}>
-                  <CssVarsProvider>
-                     {activeUsers.length !== 0 ? (
-                        activeUsers.map((ele, index) => {
-                           return (
-                              <Card key={index} variant="outlined" className={"card"}>
-                                 <CardOverflow>
-                                    <AspectRatio ratio="1">
-                                       <img src={ele.memberImage} alt="" />
-                                    </AspectRatio>
-                                 </CardOverflow>
-                                 <CardOverflow>
-                                    <Typography className={"member-nickname"}>
-                                       {ele.memberNick}
-                                    </Typography>
-                                 </CardOverflow>
-                              </Card>
-                           );
-                        })
-                     ) : (
-                        <Box className="no-data">No Active Users!</Box>
-                     )}
-                  </CssVarsProvider>
-               </Stack>
-            </Stack>
-         </Container>
-      </div>
-   );
+  const { topUsers } = useSelector(topUsersRetriever);
+  return (
+    <div className={"active-users-frame"}>
+      <Container>
+        <Stack className={"main"}>
+          <Box className={"category-title"}>Active Users</Box>
+          <Stack className={"cards-frame"}>
+            <CssVarsProvider>
+              {topUsers.length !== 0 ? (
+                topUsers.map((member: Member) => {
+                  const imagePath = `${serverApi}/${member.memberImage}`;
+                  return (
+                    <Card
+                      key={member._id}
+                      variant="outlined"
+                      className={"card"}
+                    >
+                      <CardOverflow>
+                        <AspectRatio ratio="1">
+                          <img src={imagePath} alt="" />
+                        </AspectRatio>
+                      </CardOverflow>
+                      <CardOverflow>
+                        <Typography className={"member-nickname"}>
+                          {member.memberNick}
+                        </Typography>
+                      </CardOverflow>
+                    </Card>
+                  );
+                })
+              ) : (
+                <Box className="no-data">No Active Users!</Box>
+              )}
+            </CssVarsProvider>
+          </Stack>
+        </Stack>
+      </Container>
+    </div>
+  );
 }
