20a15a9f0da883baf8ee933710636c513f69fa77
MUKHAMMADYUSUF <mfdev98@gmail.com>
2024-08-07 13:08:39 +0900

    feat: publish authentication modal component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,5 @@
+import { useState } from "react";
 import { Route, Switch, useLocation } from "react-router-dom";
-import "../css/app.css";
-import "../css/footer.css";
-import "../css/navbar.css";
 import Footer from "./components/footer";
 import HomeNavbar from "./components/headers/HomeNavbar";
 import OtherNavbar from "./components/headers/OtherNavbar";
@@ -9,13 +7,26 @@ import HelpPage from "./screens/helpPage";
 import OrdersPage from "./screens/ordersPage";
 import ProductsPage from "./screens/productsPage";
 import UserPage from "./screens/userPage";
-import Test from "./screens/Test";
 import HomePage from "./screens/homePage";
 import useBasket from "./hooks/useBasket";
+import AuthenticationModal from "./components/auth";
+import Test from "./screens/Test";
+import "../css/app.css";
+import "../css/footer.css";
+import "../css/navbar.css";
 
 function App() {
   const location = useLocation();
   const { cartItems, onAdd, onRemove, onDelete, onDeleteAll } = useBasket();
+  const [signupOpen, setSignupOpen] = useState<boolean>(false);
+  const [loginOpen, setLoginOpen] = useState<boolean>(false);
+
+  /** HANDLERS */
+  const handleSignupClose = () => setSignupOpen(false);
+  const handleLoginClose = () => setLoginOpen(false);
+
+
+
   return (
     <>
       {location.pathname === "/" ? (
@@ -54,6 +65,13 @@ function App() {
         </Route>
       </Switch>
       <Footer />
+
+      <AuthenticationModal
+        signupOpen={signupOpen}
+        loginOpen={loginOpen}
+        handleLoginClose={handleLoginClose}
+        handleSignupClose={handleSignupClose}
+      />
     </>
   );
 }
